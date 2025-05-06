f5de10874f46d357c4b6e187a009df5feca6e9a1
Kimsangmook <uj03109@naver.com>
2025-05-06 20:52:41 +0900

    chore: set up custom HTTPS dev server and update npm scripts

---
+++ src/lib/constant/index.ts
@@ -0,0 +1,7 @@
+export const ERROR_DICT: { [key: string]: string } = {
+  "Please enter a nickname": "닉네임을 입력해주세요.",
+  "nickname exists": "이미 사용 중인 닉네임입니다.",
+  "email exists": "이미 사용 중인 이메일입니다.",
+  '"email" must be a valid email': "잘못된 이메일 형식입니다.",
+  '"email" is not allowed to be empty': "이메일을 입력해주세요.",
+};
