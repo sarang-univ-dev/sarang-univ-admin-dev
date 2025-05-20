bc1f4649103ef4f2e1d3a398ae353c8370497f61
Kimsangmook <uj03109@naver.com>
2025-05-20 23:05:15 +0900

    fix: type 작업

--- src/lib/types/common.ts
+++ src/lib/types/common.ts
@@ -4,3 +4,23 @@ export interface IconProps extends ComponentProps<"svg"> {
   size?: number | string;
   fill?: string;
 }
+
+export interface IAuth {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export enum Gender {
+  MALE = "MALE",
+  FEMALE = "FEMALE",
+}
+
+export type TUser = {
+  id: number;
+  name: string;
+  phoneNumber: string;
+  gender: Gender;
+  gradeId: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
