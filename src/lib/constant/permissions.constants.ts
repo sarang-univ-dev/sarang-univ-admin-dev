c5e421ff755a5370ac1a99519fb2e9868bbfd0f6
heeseopark <heeseopark@korea.ac.kr>
2025-05-27 00:43:35 +0900

    fix: change permission and add account staff path

--- src/lib/constant/permissions.constants.ts
+++ src/lib/constant/permissions.constants.ts
@@ -9,63 +9,31 @@ export enum PagePath {
   UNIV_GROUP_BUS = '/univ-group-staff-bus',
   BUS_SCHEDULE_HISTORY = '/bus-schedule-change-history',
   BUS_SCHEDULE_CHANGE = '/bus-schedule-change-request',
+  ACCOUNT_STAFF = '/account-staff',
 }
 
 export const USER_ROLE_PAGES: Readonly<Record<UserRole, PagePath[]>> = {
   [UserRole.ACCOUNT_STAFF]: [
     PagePath.SCHEDULE_HISTORY,
     PagePath.SCHEDULE_CHANGE,
-    PagePath.COMFIRM_PAYMENT,
-    PagePath.CONFIRM_BUS_PAYMENT,
-    PagePath.UNIV_GROUP_BUS,
-    PagePath.BUS_SCHEDULE_HISTORY,
-    PagePath.BUS_SCHEDULE_CHANGE
-
+    PagePath.ACCOUNT_STAFF
   ],
   [UserRole.UNIV_GROUP_ACCOUNT_MEMBER]: [
     PagePath.COMFIRM_PAYMENT,
-    PagePath.CONFIRM_BUS_PAYMENT,
-    PagePath.UNIV_GROUP_BUS,
-    PagePath.BUS_SCHEDULE_HISTORY,
-    PagePath.BUS_SCHEDULE_CHANGE
   ],
   
   [UserRole.UNIV_GROUP_ADMIN_STAFF]: [
     PagePath.UNIV_GROUP_RETREAT,
-    PagePath.UNIV_GROUP_BUS,
-    PagePath.CONFIRM_BUS_PAYMENT,
-    PagePath.BUS_SCHEDULE_HISTORY,
-    PagePath.BUS_SCHEDULE_CHANGE
   ],
 
   [UserRole.UNIV_GROUP_LINEUP_MEMBER]:      [
-    PagePath.CONFIRM_BUS_PAYMENT,
-    PagePath.UNIV_GROUP_BUS,
-    PagePath.BUS_SCHEDULE_HISTORY,
-    PagePath.BUS_SCHEDULE_CHANGE
   ],  
   [UserRole.LINEUP_STAFF]:                  [
-    PagePath.CONFIRM_BUS_PAYMENT,
-    PagePath.UNIV_GROUP_BUS,
-    PagePath.BUS_SCHEDULE_HISTORY,
-    PagePath.BUS_SCHEDULE_CHANGE
   ],
   [UserRole.UNIV_GROUP_DORMITORY_MEMBER]:   [
-    PagePath.CONFIRM_BUS_PAYMENT,
-    PagePath.UNIV_GROUP_BUS,
-    PagePath.BUS_SCHEDULE_HISTORY,
-    PagePath.BUS_SCHEDULE_CHANGE
   ],
   [UserRole.DORMITORY_STAFF]:               [
-    PagePath.CONFIRM_BUS_PAYMENT,
-    PagePath.UNIV_GROUP_BUS,
-    PagePath.BUS_SCHEDULE_HISTORY,
-    PagePath.BUS_SCHEDULE_CHANGE
   ],
   [UserRole.SHUTTLE_BUS_BOARDING_STAFF]:    [
-    PagePath.CONFIRM_BUS_PAYMENT,
-    PagePath.UNIV_GROUP_BUS,
-    PagePath.BUS_SCHEDULE_HISTORY,
-    PagePath.BUS_SCHEDULE_CHANGE
   ],
 };
