f6ef8fe628bb3b3649a5b4f87d878c46063bf556
heeseopark <heeseopark@korea.ac.kr>
2025-05-27 00:44:53 +0900

    feat: add home page text

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,14 @@
 const Home = () => {
-  return <></>;
+  return (
+    <div className="flex min-h-screen">
+      <main className="flex-1 p-8">
+        <h1 className="text-2xl font-bold mb-4">
+          사랑의교회 대학부 관리자 페이지
+        </h1>
+        <p>사이드바를 통해 원하는 페이지로 이동해주세요.</p>
+      </main>
+    </div>
+  );
 };
 
 export default Home;
